355e52a8a3e11e2e48a8fba2671ebea4c0e73c44
tiarmagurning <64045845+tiarmaGurning@users.noreply.github.com>
2020-05-11 10:38:49 +0700

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,4 @@
+<?php 
+
+define('BASEURL', 'http://localhost/phpmvc/public');
+
---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php 
+
+class User_model {
+    private $nama = 'Tiarma Gurning';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
